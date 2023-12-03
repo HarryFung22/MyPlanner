6f48294f1cb0b4dcf2d5be360f091551d5003cc1
Harry <harryfung22@gmail.com>
2023-12-03 17:28:56 -0500

    apply migrations

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User, AbstractUser
 
 # Create your models here.
 class Note(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Establishes a ForeignKey relationship with the User model
+    user = models.TextField(null=True, blank=False)
     body = models.TextField(null=True, blank=False)
     updated = models.DateTimeField(auto_now=True)       
     created = models.DateTimeField(auto_now_add=True)   
