f2670e64c04504b5cc63a4996439c245ed7b24b4
Harry <harryfung22@gmail.com>
2023-12-10 21:18:23 -0500

    add overflow

--- frontend/src/pages/Note.js
+++ frontend/src/pages/Note.js
@@ -22,7 +22,11 @@ const Note = ({ note, setRerender }) => {
   return (
     <div className="bg-yellow-200 p-4 rounded-md shadow-md w-48 h-48 relative">
       <div className="flex flex-col justify-center items-center h-full">
-        <div className="mb-4 text-center">{note.body}</div>
+        <div className="mb-4 text-center overflow-hidden max-h-full max-w-full whitespace-nowrap overflow-ellipsis">
+          <span className="inline-block w-full">
+            <div className="px-2">{note.body}</div>
+          </span>
+        </div>
         <div className="absolute top-2 right-2">
           <FontAwesomeIcon icon={faPenToSquare} onClick={() => navigate(`/update/${note.id}`)} className='mr-2'/>
           <FontAwesomeIcon icon={faTrash} onClick={() => handleDelete()}/>
