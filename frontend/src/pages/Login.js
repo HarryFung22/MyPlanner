880652eb6ed40840792ae7f34a9ca4ed2615d2a5
Harry <harryfung22@gmail.com>
2023-12-22 20:08:16 -0500

    add token on req

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -12,10 +12,13 @@ const Login = () => {
     const navigate = useNavigate()
 
     const handleSubmit = async (e) => {
+        const token = localStorage.getItem('token')
+        
         const response = await fetch(`http://localhost:8000/api/token/`, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
             },
             body: JSON.stringify({username: userName, password: password}),
         })
